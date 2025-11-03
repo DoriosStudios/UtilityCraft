1dc94f61c0833ddd1c390b2315bc5c3375f70622
Dorios Studios <chavez100577@gmail.com>
2025-11-03 12:34:47 -0600

    Fixed Sieve Drops
    - Some drops had wrong chances (Way Higher than they Should)

--- BP/scripts/config/recipes/sieve.js
+++ BP/scripts/config/recipes/sieve.js
@@ -45,12 +45,12 @@ export const acceptedBlocks = [
 export const sieveRecipes = {
     'minecraft:gravel': [
         { item: 'minecraft:flint', amount: 1, chance: 0.20, tier: 0 },
-        { item: 'utilitycraft:iron_chunk', amount: 1, chance: 0.20, tier: 1 },
-        { item: 'utilitycraft:coal_chunk', amount: 1, chance: 0.30, tier: 0 },
-        { item: 'utilitycraft:gold_chunk', amount: 1, chance: 0.10, tier: 3 },
-        { item: 'utilitycraft:lapislazuli_chunk', amount: 1, chance: 0.2, tier: 3 },
-        { item: 'utilitycraft:emerald_chunk', amount: 1, chance: 0.2, tier: 4 },
-        { item: 'utilitycraft:diamond_chunk', amount: 1, chance: 0.15, tier: 4 }
+        { item: 'utilitycraft:iron_chunk', amount: 1, chance: 0.15, tier: 1 },
+        { item: 'utilitycraft:coal_chunk', amount: 1, chance: 0.25, tier: 0 },
+        { item: 'utilitycraft:gold_chunk', amount: 1, chance: 0.05, tier: 3 },
+        { item: 'utilitycraft:lapislazuli_chunk', amount: 1, chance: 0.025, tier: 3 },
+        { item: 'utilitycraft:emerald_chunk', amount: 1, chance: 0.02, tier: 4 },
+        { item: 'utilitycraft:diamond_chunk', amount: 1, chance: 0.01, tier: 4 }
     ],
     'minecraft:dirt': [
         { item: 'minecraft:carrot', amount: 1, chance: 0.10 },
@@ -83,7 +83,7 @@ export const sieveRecipes = {
         { item: 'utilitycraft:nether_quartz_chunk', amount: 1, chance: 0.33, tier: 1 },
         { item: 'minecraft:gold_nugget', amount: 1, chance: 0.20, tier: 3 },
         { item: 'utilitycraft:nether_gold_chunk', amount: 1, chance: 0.33, tier: 3 },
-        { item: 'utilitycraft:ancient_debris_chunk', amount: 1, chance: 0.25, tier: 5 }
+        { item: 'utilitycraft:ancient_debris_chunk', amount: 1, chance: 0.025, tier: 5 }
     ],
     'minecraft:sand': [
         { item: 'minecraft:prismarine_shard', amount: 1, chance: 0.10, tier: 2 },
@@ -92,38 +92,38 @@ export const sieveRecipes = {
         { item: 'utilitycraft:redstone_chunk', amount: 1, chance: 0.20, tier: 2 },
         { item: 'minecraft:bone_meal', amount: 1, chance: 0.25 },
         { item: 'minecraft:gunpowder', amount: 1, chance: 0.12 },
-        { item: 'minecraft:glowstone_dust', amount: 1, chance: 0.8 },
+        { item: 'minecraft:glowstone_dust', amount: 1, chance: 0.08 },
         { item: 'minecraft:blaze_powder', amount: 1, chance: 0.10, tier: 3 },
         { item: 'minecraft:cactus', amount: 1, chance: 0.10 },
         { item: 'minecraft:kelp', amount: 1, chance: 0.10 },
         { item: 'minecraft:clay_ball', amount: 1, chance: 0.10, tier: 2 },
-        { item: 'minecraft:cocoa_beans', amount: 1, chance: 0.1 },
+        { item: 'minecraft:cocoa_beans', amount: 1, chance: 0.01 },
         { item: 'minecraft:heart_of_the_sea', amount: 1, chance: 0.01, tier: 4 },
         { item: 'minecraft:conduit', amount: 1, chance: 0.01, tier: 3 },
         // Integrated Storage
         { item: 'ae2be:certus_quartz_crystal', amount: 1, chance: 0.17, tier: 3 },
-        { item: 'ae2be:charged_certus_quartz_crystal', amount: 1, chance: 0.1, tier: 3 }
+        { item: 'ae2be:charged_certus_quartz_crystal', amount: 1, chance: 0.01, tier: 3 }
     ],
     'minecraft:soul_sand': [
         { item: 'utilitycraft:nether_quartz_chunk', amount: 1, chance: 0.33, tier: 1 },
         { item: 'utilitycraft:nether_quartz_chunk', amount: 3, chance: 0.10, tier: 1 },
         { item: 'minecraft:bone', amount: 1, chance: 0.15 },
-        { item: 'minecraft:ghast_tear', amount: 1, chance: 0.8, tier: 4 },
+        { item: 'minecraft:ghast_tear', amount: 1, chance: 0.08, tier: 4 },
         { item: 'minecraft:nether_wart', amount: 1, chance: 0.12 },
         { item: 'minecraft:warped_fungus', amount: 1, chance: 0.10 },
         { item: 'minecraft:crimson_fungus', amount: 1, chance: 0.10 }
     ],
     'utilitycraft:crushed_endstone': [
-        { item: 'minecraft:chorus_flower', amount: 1, chance: 0.1, tier: 4 },
+        { item: 'minecraft:chorus_flower', amount: 1, chance: 0.01, tier: 4 },
         { item: 'minecraft:chorus_fruit', amount: 1, chance: 0.80, tier: 4 },
         { item: 'minecraft:ender_pearl', amount: 1, chance: 0.16, tier: 4 }
     ],
     'utilitycraft:crushed_cobbled_deepslate': [
-        { item: 'minecraft:echo_shard', amount: 1, chance: 0.5, tier: 5 },
-        { item: 'minecraft:sculk_catalyst', amount: 1, chance: 0.05, tier: 5 },
-        { item: 'minecraft:amethyst_shard', amount: 1, chance: 0.1, tier: 5 },
-        { item: 'utilitycraft:deepslate_diamond_chunk', amount: 1, chance: 0.5, tier: 4 },
-        { item: 'utilitycraft:deepslate_emerald_chunk', amount: 1, chance: 0.5, tier: 4 },
+        { item: 'minecraft:echo_shard', amount: 1, chance: 0.05, tier: 5 },
+        { item: 'minecraft:sculk_catalyst', amount: 1, chance: 0.005, tier: 5 },
+        { item: 'minecraft:amethyst_shard', amount: 1, chance: 0.01, tier: 5 },
+        { item: 'utilitycraft:deepslate_diamond_chunk', amount: 1, chance: 0.05, tier: 4 },
+        { item: 'utilitycraft:deepslate_emerald_chunk', amount: 1, chance: 0.05, tier: 4 },
         { item: 'utilitycraft:deepslate_gold_chunk', amount: 1, chance: 0.20, tier: 4 },
         { item: 'utilitycraft:deepslate_iron_chunk', amount: 1, chance: 0.25, tier: 1 },
         { item: 'utilitycraft:deepslate_lapislazuli_chunk', amount: 1, chance: 0.15, tier: 3 },
@@ -132,12 +132,12 @@ export const sieveRecipes = {
     //Compressed
     'utilitycraft:compressed_gravel': [
         { item: 'minecraft:flint', amount: 9, chance: 0.20, tier: 0 },
-        { item: 'utilitycraft:iron_chunk', amount: 9, chance: 0.20, tier: 1 },
-        { item: 'utilitycraft:coal_chunk', amount: 9, chance: 0.30, tier: 0 },
-        { item: 'utilitycraft:gold_chunk', amount: 9, chance: 0.10, tier: 3 },
-        { item: 'utilitycraft:lapislazuli_chunk', amount: 9, chance: 0.2, tier: 3 },
-        { item: 'utilitycraft:emerald_chunk', amount: 9, chance: 0.2, tier: 4 },
-        { item: 'utilitycraft:diamond_chunk', amount: 9, chance: 0.15, tier: 4 }
+        { item: 'utilitycraft:iron_chunk', amount: 9, chance: 0.15, tier: 1 },
+        { item: 'utilitycraft:coal_chunk', amount: 9, chance: 0.25, tier: 0 },
+        { item: 'utilitycraft:gold_chunk', amount: 9, chance: 0.05, tier: 3 },
+        { item: 'utilitycraft:lapislazuli_chunk', amount: 9, chance: 0.025, tier: 3 },
+        { item: 'utilitycraft:emerald_chunk', amount: 9, chance: 0.02, tier: 4 },
+        { item: 'utilitycraft:diamond_chunk', amount: 9, chance: 0.01, tier: 4 }
     ],
     'utilitycraft:compressed_dirt': [
         { item: 'minecraft:carrot', amount: 9, chance: 0.10 },
@@ -165,37 +165,37 @@ export const sieveRecipes = {
         { item: 'utilitycraft:redstone_chunk', amount: 9, chance: 0.20, tier: 2 },
         { item: 'minecraft:bone_meal', amount: 9, chance: 0.25 },
         { item: 'minecraft:gunpowder', amount: 9, chance: 0.12 },
-        { item: 'minecraft:glowstone_dust', amount: 9, chance: 0.8 },
+        { item: 'minecraft:glowstone_dust', amount: 9, chance: 0.08 },
         { item: 'minecraft:blaze_powder', amount: 9, chance: 0.10, tier: 3 },
         { item: 'minecraft:cactus', amount: 9, chance: 0.10 },
         { item: 'minecraft:kelp', amount: 9, chance: 0.10 },
         { item: 'minecraft:clay_ball', amount: 9, chance: 0.10, tier: 2 },
-        { item: 'minecraft:cocoa_beans', amount: 9, chance: 0.1 },
-        { item: 'minecraft:heart_of_the_sea', amount: 9, chance: 0.01, tier: 3 },
+        { item: 'minecraft:cocoa_beans', amount: 9, chance: 0.01 },
+        { item: 'minecraft:heart_of_the_sea', amount: 9, chance: 0.01, tier: 4 },
         { item: 'minecraft:conduit', amount: 9, chance: 0.01, tier: 3 },
         // Integrated Storage
         { item: 'ae2be:certus_quartz_crystal', amount: 9, chance: 0.17, tier: 3 },
-        { item: 'ae2be:charged_certus_quartz_crystal', amount: 9, chance: 0.1, tier: 3 }
+        { item: 'ae2be:charged_certus_quartz_crystal', amount: 9, chance: 0.01, tier: 3 }
     ],
     'utilitycraft:compressed_crushed_netherrack': [
         { item: 'utilitycraft:nether_quartz_chunk', amount: 9, chance: 0.33, tier: 1 },
         { item: 'minecraft:gold_nugget', amount: 9, chance: 0.20, tier: 3 },
         { item: 'utilitycraft:nether_gold_chunk', amount: 9, chance: 0.33, tier: 3 },
-        { item: 'utilitycraft:ancient_debris_chunk', amount: 9, chance: 0.25, tier: 5 }
+        { item: 'utilitycraft:ancient_debris_chunk', amount: 9, chance: 0.025, tier: 5 }
     ],
     'utilitycraft:compressed_crushed_cobbled_deepslate': [
-        { item: 'minecraft:echo_shard', amount: 9, chance: 0.5, tier: 5 },
-        { item: 'minecraft:sculk_catalyst', amount: 9, chance: 0.05, tier: 5 },
-        { item: 'minecraft:amethyst_shard', amount: 9, chance: 0.1, tier: 5 },
-        { item: 'utilitycraft:deepslate_diamond_chunk', amount: 9, chance: 0.5, tier: 4 },
-        { item: 'utilitycraft:deepslate_emerald_chunk', amount: 9, chance: 0.5, tier: 4 },
+        { item: 'minecraft:echo_shard', amount: 9, chance: 0.05, tier: 5 },
+        { item: 'minecraft:sculk_catalyst', amount: 9, chance: 0.005, tier: 5 },
+        { item: 'minecraft:amethyst_shard', amount: 9, chance: 0.01, tier: 5 },
+        { item: 'utilitycraft:deepslate_diamond_chunk', amount: 9, chance: 0.05, tier: 4 },
+        { item: 'utilitycraft:deepslate_emerald_chunk', amount: 9, chance: 0.05, tier: 4 },
         { item: 'utilitycraft:deepslate_gold_chunk', amount: 9, chance: 0.20, tier: 4 },
         { item: 'utilitycraft:deepslate_iron_chunk', amount: 9, chance: 0.25, tier: 1 },
         { item: 'utilitycraft:deepslate_lapislazuli_chunk', amount: 9, chance: 0.15, tier: 3 },
         { item: 'utilitycraft:deepslate_coal_chunk', amount: 9, chance: 0.30, tier: 0 }
     ],
     'utilitycraft:compressed_crushed_endstone': [
-        { item: 'minecraft:chorus_flower', amount: 9, chance: 0.1, tier: 4 },
+        { item: 'minecraft:chorus_flower', amount: 9, chance: 0.01, tier: 4 },
         { item: 'minecraft:chorus_fruit', amount: 9, chance: 0.80, tier: 4 },
         { item: 'minecraft:ender_pearl', amount: 9, chance: 0.16, tier: 4 }
     ]
